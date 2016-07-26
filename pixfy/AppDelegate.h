acb8ccde48e337c62eaadca55a91ddb02a47e1b9
bigchou <40347905S@gmail.com>
2016-07-26 16:12:02 +0800

    Initial Commit

---
+++ pixfy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pixfy
+//
+//  Created by Timmy on 2016/7/26.
+//  Copyright © 2016年 bigchou image. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
