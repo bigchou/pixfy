0844c1effe0d26b1c9c4d3e3a55a8801aa7ca471
bigchou <40347905S@gmail.com>
2016-07-28 14:48:17 +0800

    Reset

--- pixfy/ViewController.mm
+++ pixfy/ViewController.mm
@@ -15,8 +15,35 @@
 @end
 
 @implementation ViewController
+- (IBAction)Reset:(UIButton *)sender {
+    _MainImage.image = origin;
+}
+
 - (IBAction)Apply:(UIButton *)sender {
-    cv::Mat img = [UIImageCVMatConverter cvMatGrayFromUIImage:_MainImage.image];
+    NSLog(@"1");
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Effect Selection"
+                                                                   message:@"Please select the effect"
+                                                            preferredStyle:UIAlertControllerStyleActionSheet]; // 1
+    UIAlertAction *firstAction = [UIAlertAction actionWithTitle:@"GrayScale"
+                                                          style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                                                              NSLog(@"You pressed \"GrayScale\"");
+                                                              cv::Mat img=[UIImageCVMatConverter cvMatGrayFromUIImage:_MainImage.image];
+                                                              _MainImage.image = [UIImageCVMatConverter UIImageFromCVMat:img];
+                                                          }]; // 2
+    
+    
+    UIAlertAction *secondAction = [UIAlertAction actionWithTitle:@"two"
+                                                           style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                                                               NSLog(@"You pressed button two");
+                                                               _MainImage.image = origin;
+                                                           }]; // 3
+    
+    [alert addAction:firstAction]; // 4
+    [alert addAction:secondAction]; // 5
+    
+    [self presentViewController:alert animated:YES completion:nil]; // 6
+    NSLog(@"3");
+    /*cv::Mat img = [UIImageCVMatConverter cvMatGrayFromUIImage:_MainImage.image];
     printf("rows: %d\tcols: %d\t",img.rows,img.cols);
     // main processing start
     for(int i = 0; i < img.rows; i++){
@@ -26,12 +53,15 @@
         }
     }
     // main processing end
-    _MainImage.image = [UIImageCVMatConverter UIImageFromCVMat:img];
+    _MainImage.image = [UIImageCVMatConverter UIImageFromCVMat:img];*/
 }
 
+
+UIImage *origin = NULL;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    origin  = _MainImage.image;
 }
 
 - (void)didReceiveMemoryWarning {
